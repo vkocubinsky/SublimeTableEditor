e4051b11897c935723e7a33240298cf34fc899a9
Valery Kocubinsky <valery.kocubinsky@gmail.com>
2013-03-20 19:52:43 +0200

    introduce TableContext:insert hline and move

--- table_plugin.py
+++ table_plugin.py
@@ -669,15 +669,15 @@ class TableEditorHlineAndMove(AbstractTableMultiSelect):
     """
 
     def run_one_sel(self, edit, sel):
-        (sel_row, sel_col) = self.view.rowcol(sel.begin())
-        field_num = self.get_unformatted_field_num(sel_row, sel_col)
-        first_table_row = self.get_first_table_row(sel_row)
-        last_table_row = self.get_last_table_row(sel_row)
-        table_text = self.get_table_text(first_table_row, last_table_row)
-        table = tablelib.TextTable(table_text, self.syntax)
-        self.merge(edit, first_table_row,last_table_row, table.render_lines())
+        ctx = TableContext(self.view, sel, self.syntax)
+        table = tablelib.TextTable(ctx.table_text, self.syntax)
 
-        row_num = sel_row - first_table_row
+        sel_row = ctx.sel_row
+        field_num = ctx.field_num
+
+        self.merge(edit, ctx.first_table_row,ctx.last_table_row, table.render_lines())
+
+        row_num = sel_row - ctx.first_table_row
         table.insert_single_separator_row(row_num + 1)
 
         if row_num + 2 < table.row_count:
@@ -685,8 +685,7 @@ class TableEditorHlineAndMove(AbstractTableMultiSelect):
                 table.insert_empty_row(row_num + 2)
         else:
             table.insert_empty_row(row_num + 2)
-        self.merge(edit, first_table_row,last_table_row, table.render_lines())
-        print table.render()
+        self.merge(edit, ctx.first_table_row,ctx.last_table_row, table.render_lines())
 
         sel_row = sel_row + 2
         pt = self.get_field_default_point(sel_row, 0)
