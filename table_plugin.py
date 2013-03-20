d8c524cb625c87eb3cab7bd316f9ab31c1f09341
Valery Kocubinsky <valery.kocubinsky@gmail.com>
2013-03-20 19:44:10 +0200

    introduce TableContext:kill row

--- table_plugin.py
+++ table_plugin.py
@@ -540,18 +540,18 @@ class TableEditorKillRow(AbstractTableMultiSelect):
     """
 
     def run_one_sel(self, edit, sel):
-        (sel_row, sel_col) = self.view.rowcol(sel.begin())
-        field_num = self.get_unformatted_field_num(sel_row, sel_col)
-        first_table_row = self.get_first_table_row(sel_row)
-        last_table_row = self.get_last_table_row(sel_row)
-        table_text = self.get_table_text(first_table_row, last_table_row)
-        table = tablelib.TextTable(table_text, self.syntax)
-        table.delete_row(sel_row - first_table_row)
-        self.merge(edit,first_table_row, last_table_row, table.render_lines())
-        if first_table_row == last_table_row:
+        ctx = TableContext(self.view, sel, self.syntax)
+        table = tablelib.TextTable(ctx.table_text, self.syntax)
+
+        sel_row = ctx.sel_row
+        field_num = ctx.field_num
+
+        table.delete_row(sel_row - ctx.first_table_row)
+        self.merge(edit,ctx.first_table_row, ctx.last_table_row, table.render_lines())
+        if ctx.first_table_row == ctx.last_table_row:
             pt = self.view.text_point(sel_row, 0)
         else:
-            if sel_row == last_table_row:
+            if sel_row == ctx.last_table_row:
                 sel_row = sel_row - 1
             pt = self.get_field_default_point(sel_row, field_num)
         return sublime.Region(pt, pt)
