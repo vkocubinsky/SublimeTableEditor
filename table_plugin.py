82130a18ae0981ba005fc958851ec16c7d46fa79
Valery Kocubinsky <valery.kocubinsky@gmail.com>
2013-03-20 19:28:46 +0200

    introduce TableContext:next row

--- table_plugin.py
+++ table_plugin.py
@@ -427,21 +427,20 @@ class TableEditorNextRow(AbstractTableMultiSelect):
     """
 
     def run_one_sel(self, edit, sel):
-        (sel_row, sel_col) = self.view.rowcol(sel.begin())
-        field_num = self.get_unformatted_field_num(sel_row, sel_col)
-        first_table_row = self.get_first_table_row(sel_row)
-        last_table_row = self.get_last_table_row(sel_row)
-        table_text = self.get_table_text(first_table_row, last_table_row)
-        table = tablelib.TextTable(table_text, self.syntax)
-        self.merge(edit, first_table_row,last_table_row, table.render_lines())
+        ctx = TableContext(self.view, sel, self.syntax)
+        table = tablelib.TextTable(ctx.table_text, self.syntax)
+        self.merge(edit, ctx.first_table_row,ctx.last_table_row, table.render_lines())
 
-        if sel_row < last_table_row:
-            if table[sel_row - first_table_row + 1].is_separator():
-                table.insert_empty_row(sel_row - first_table_row + 1)
-                self.merge(edit, first_table_row,last_table_row, table.render_lines())
+        sel_row = ctx.sel_row
+        field_num = ctx.field_num
+
+        if sel_row < ctx.last_table_row:
+            if table[sel_row - ctx.first_table_row + 1].is_separator():
+                table.insert_empty_row(sel_row - ctx.first_table_row + 1)
+                self.merge(edit, ctx.first_table_row,ctx.last_table_row, table.render_lines())
         else:
             table.insert_empty_row(table.row_count)
-            self.merge(edit, first_table_row,last_table_row, table.render_lines())
+            self.merge(edit, ctx.first_table_row,ctx.last_table_row, table.render_lines())
         sel_row += 1
         pt = self.get_field_default_point(sel_row, field_num)
         return sublime.Region(pt, pt)
