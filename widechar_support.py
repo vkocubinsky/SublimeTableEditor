5dcc87b127215f577e97630a30850efddb00d856
Valery Kocubinsky <valery.kocubinsky@gmail.com>
2013-09-04 18:32:43 +0300

    cjk: add references to wiki, add so0me ranges

--- widechar_support.py
+++ widechar_support.py
@@ -2,7 +2,7 @@
 
 # Copyright (C) 2013  Free Software Foundation, Inc.
 
-# Author: Zealic Zeng
+# Author: Zealic Zeng, Valery Kocubinsky
 # Package: SublimeTableEditor
 # Homepage: https://github.com/zealic/contrib-SublimeTableEditor
 
@@ -22,26 +22,34 @@
 # along with SublimeTableEditor.  If not, see <http://www.gnu.org/licenses/>.
 
 
-# Reference - http://svn.edgewall.org/repos/trac/trunk/trac/util/text.py
 breakable_char_ranges = [
-    (0x1100, 0x11FF),   # Hangul Jamo
+    #http://en.wikipedia.org/wiki/Han_unification
+    (0x4E00, 0x9FFF),   # CJK Unified Ideographs
+    (0x3400, 0x4DBF),   # CJK Unified Ideographs Extension A
+    (0xF900, 0xFAFF),   # CJK Compatibility Ideographs
     (0x2E80, 0x2EFF),   # CJK Radicals Supplement
     (0x3000, 0x303F),   # CJK Symbols and Punctuation
-    (0x3040, 0x309F),   # Hiragana
-    (0x30A0, 0x30FF),   # Katakana
-    (0x3130, 0x318F),   # Hangul Compatibility Jamo
-    (0x3190, 0x319F),   # Kanbun
     (0x31C0, 0x31EF),   # CJK Strokes
+    (0x2FF0, 0x2FFF),   # Ideographic Description Characters
+    (0x2F00, 0x2FDF),   # Kangxi Radicals
     (0x3200, 0x32FF),   # Enclosed CJK Letters and Months
     (0x3300, 0x33FF),   # CJK Compatibility
-    (0x3400, 0x4DBF),   # CJK Unified Ideographs Extension A
-    (0x4E00, 0x9FFF),   # CJK Unified Ideographs
-    (0xA960, 0xA97F),   # Hangul Jamo Extended-A
-    (0xAC00, 0xD7AF),   # Hangul Syllables
-    (0xD7B0, 0xD7FF),   # Hangul Jamo Extended-B
     (0xF900, 0xFAFF),   # CJK Compatibility Ideographs
     (0xFE30, 0xFE4F),   # CJK Compatibility Forms
-    (0xFF00, 0xFFEF),   # Halfwidth and Fullwidth Forms
+    # See http://en.wikipedia.org/wiki/Hiragana
+    (0x3040, 0x309F),   # Hiragana
+    # See http://en.wikipedia.org/wiki/Katakana
+    (0x30A0, 0x30FF),   # Katakana
+    # See http://en.wikipedia.org/wiki/Hangul
+    (0x1100, 0x11FF),   # Hangul Jamo
+    (0x3130, 0x318F),   # Hangul Compatibility Jamo
+    (0xA960, 0xA97F),   # Hangul Jamo Extended-A
+    (0xD7B0, 0xD7FF),   # Hangul Jamo Extended-B
+    (0xAC00, 0xD7A3),   # Hangul syllables
+    # See http://en.wikipedia.org/wiki/Kanbun
+    (0x3190, 0x319F),   # Kanbun
+    # See http://en.wikipedia.org/wiki/Halfwidth_and_Fullwidth_Forms
+    (0xFF00, 0xFFEF),    # Halfwidth and Fullwidth Forms
 ]
 
 
@@ -59,7 +67,6 @@ def _is_widechar(c):
 
 
 def _norm_text(text):
-    import sys
     import locale
     if not isinstance(text, unicode):
         text = unicode(text, locale.getpreferredencoding())
